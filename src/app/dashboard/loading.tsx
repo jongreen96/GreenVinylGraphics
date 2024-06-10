2575ed5198c816d6f2d94ad2f16797c1f1e22539
Jon Green <jongreen1996@gmail.com>
2024-06-10 20:57:34 +0100

    Centered loading spinner

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react';
 
 export default function DashboardLoading() {
   return (
-    <div className='flex w-full h-full items-center justify-center'>
+    <div className='flex w-full h-[90svh] items-center justify-center'>
       <Loader2 className='animate-spin size-24' />
     </div>
   );
