c45c2793a5b7acea3c1c93d0f932c74417da6327
Jon Green <jongreen1996@gmail.com>
2024-06-14 13:39:33 +0100

    Setup stripe webhook

---
+++ src/app/webhooks/stripe/route.ts
@@ -0,0 +1,57 @@
+import db from '@/db/db';
+import { NextRequest, NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export async function POST(req: NextRequest) {
+  const event = stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get('Stripe-Signature') as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+
+  if (event.type === 'charge.succeeded') {
+    const charge = event.data.object;
+    const productId = charge.metadata.product_id;
+    const email = charge.billing_details.email;
+    const pricePaidInPence = charge.amount;
+
+    const product = await db.product.findFirst({
+      where: { id: productId },
+    });
+
+    if (product === null || email === null)
+      return new NextResponse('Product not found', { status: 404 });
+
+    const userFields = {
+      email,
+      orders: {
+        create: { productId, pricePaidInPence },
+      },
+    };
+
+    const {
+      orders: [order],
+    } = await db.user.upsert({
+      where: { email },
+      create: userFields,
+      update: userFields,
+      select: {
+        orders: {
+          orderBy: { createdAt: 'desc' },
+          take: 1,
+        },
+      },
+    });
+
+    const downloadVerification = await db.downloadVerification.create({
+      data: {
+        productId,
+        expiresAt: new Date(
+          order.createdAt.getTime() + 1000 * 60 * 60 * 24 * 7
+        ),
+      },
+    });
+  }
+}
