1f51b359ac380a975027182be2b800c66490eb66
Jon Green <jongreen1996@gmail.com>
2024-06-17 17:42:53 +0100

    Add header to all products page

--- src/app/(publicPages)/products/page.tsx
+++ src/app/(publicPages)/products/page.tsx
@@ -1,3 +1,4 @@
+import { PageHeader } from '@/app/dashboard/_components/pageHeader';
 import { ProductCard, ProductCardSkeleton } from '@/components/productCard';
 import { getAllProducts } from '@/db/queries';
 import { cache } from '@/lib/cache';
@@ -7,19 +8,22 @@ const getProducts = cache(() => getAllProducts(), ['/products', 'getProducts']);
 
 export default function ProductsPage() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-      <Suspense
-        fallback={
-          <>
-            {[...Array(8)].map((_, index) => (
-              <ProductCardSkeleton key={index} />
-            ))}
-          </>
-        }
-      >
-        <ProductSuspense />
-      </Suspense>
-    </div>
+    <>
+      <PageHeader>All Products</PageHeader>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <Suspense
+          fallback={
+            <>
+              {[...Array(8)].map((_, index) => (
+                <ProductCardSkeleton key={index} />
+              ))}
+            </>
+          }
+        >
+          <ProductSuspense />
+        </Suspense>
+      </div>
+    </>
   );
 }
 
